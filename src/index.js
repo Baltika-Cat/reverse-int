184b57fd1ceba6ebf1bacb9b8539e00b7f8071f7
Baltika-Cat <Brashkin.dmitrii@gmail.com>
2023-09-26 23:06:45 +0400

    feat: success

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  const str = n.toString();
+  let result = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    result += str[i];
+  }
+  return parseInt(result, 10);
 }
